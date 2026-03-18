0538f07ff87025e812e79b824b90e61af0f4b5d1
Professor Guilherme <guilhermealonsoribeiro@gmail.com>
2026-03-18 10:25:23 -0300

    primeiros passos com JS

---
+++ main.js
@@ -0,0 +1,19 @@
+console.log("Aprendendo lógica com javascript");
+console.log("===================");
+
+/*
+let idade = 18;
+
+if (idade >= 18) {
+    console.log("maior da idade.")
+} else {
+    console.log("menor de idade")
+}
+*/
+
+let contador = 1;
+
+while (contador <= 5) {
+    console.log(contador);
+    contador++;
+}
